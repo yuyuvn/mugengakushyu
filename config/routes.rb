d9ec876c25786c1a1f3089efe2f448f000127249
Clicia Scarlet <yuyuvn@mac.com>
2015-10-12 08:06:48 +0000

    user_log

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :user_logs
   root 'static_pages#home'
 
   devise_for :users
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151005065836) do
+ActiveRecord::Schema.define(version: 20151012075915) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
     t.integer  "number_of_pages"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+  end
+
+  create_table "user_logs", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
