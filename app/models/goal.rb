71fce063fd685f57e51b08c82a6b8d15d458fed5
Clicia Scarlet <yuyuvn@mac.com>
2015-12-05 11:09:08 +0000

    Fix 2

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -2,14 +2,14 @@ class Goal < ActiveRecord::Base
   belongs_to :user
 
   def remain_word
-  	goal - user.learned_word
+  	[goal - user.learned_word,0].max
   end
 
   def day_left
-  	(deadline.to_date - Date.today).to_f
+  	[(deadline.to_date - Date.today).to_f,0].max
   end
 
   def word_average
-  	remain_word / day_left
+  	day_left > 0 ? remain_word / day_left : 0
   end
 end
