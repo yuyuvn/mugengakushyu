b464d5ccbc210d2e084a2cfd3c8e9806d16c4bc7
ndkhoa01 <khoa.hedspi.6b@gmail.com>
2015-12-06 11:02:59 +0000

    Almost almost final

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   before_validation :adjust_email, on: :create
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable
-  devise :omniauthable, :omniauth_providers => [:facebook, :twitter, :google_oauth2]       
+  devise :omniauthable, :omniauth_providers => [:google_oauth2]       
 
   has_many :results
   has_many :goals
@@ -48,48 +48,48 @@ class User < ActiveRecord::Base
     User.where(id: fid1+fid2)
   end
 
-  def self.find_for_facebook_oauth(auth, signed_in_resource = nil)
-    data = auth.info
-    user = User.where(:provider => auth.provider, :uid => auth.uid).first
-    if user
-      return user
-    else
-      registered_user = User.where(:email => auth.info.email).first
-    if registered_user
-      return registered_user
-    else
-      auth.provider = 'Facebook'
-      user = User.create!(
-        provider:auth.provider,
-        uid:auth.uid,
-        name: data["name"],
-        email:auth.info.mail,
-        password:Devise.friendly_token[0,10],
-        )
-    end
-    end
-  end
+  # def self.find_for_facebook_oauth(auth, signed_in_resource = nil)
+  #   data = auth.info
+  #   user = User.where(:provider => auth.provider, :uid => auth.uid).first
+  #   if user
+  #     return user
+  #   else
+  #     registered_user = User.where(:email => auth.info.email).first
+  #   if registered_user
+  #     return registered_user
+  #   else
+  #     auth.provider = 'Facebook'
+  #     user = User.create!(
+  #       provider:auth.provider,
+  #       uid:auth.uid,
+  #       name: data["name"],
+  #       email:auth.info.mail,
+  #       password:Devise.friendly_token[0,10],
+  #       )
+  #   end
+  #   end
+  # end
   
-  def self.find_for_twitter_oauth(auth, signed_in_resource=nil)
-    user = User.where(:provider => auth.provider, :uid => auth.uid).first
-    if user
-      return user
-    else
-      registered_user = User.where(:email => auth.info.email).first
-      if registered_user
-        return registered_user
-      else
+  # def self.find_for_twitter_oauth(auth, signed_in_resource=nil)
+  #   user = User.where(:provider => auth.provider, :uid => auth.uid).first
+  #   if user
+  #     return user
+  #   else
+  #     registered_user = User.where(:email => auth.info.email).first
+  #     if registered_user
+  #       return registered_user
+  #     else
 
-        user = User.create(name:auth.extra.raw_info.name,
-                            provider:auth.provider,
-                            uid:auth.uid,
-                            email:auth.info.email,
-                            password:Devise.friendly_token[0,10],
-                          )
-      end
+  #       user = User.create(name:auth.extra.raw_info.name,
+  #                           provider:auth.provider,
+  #                           uid:auth.uid,
+  #                           email:auth.info.email,
+  #                           password:Devise.friendly_token[0,10],
+  #                         )
+  #     end
 
-    end
-  end
+  #   end
+  # end
   
   def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
     data = access_token.info
